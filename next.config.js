d6660d4fa495fdd1147b336e2ebb10b7c5484fb0
Stephen <night-build@kali.stephen>
2023-04-07 09:24:53 +0100

    mapbox api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
+const webpack = require('webpack');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  webpack: config => {
+    // Optional: Enables reading mapbox token from environment variable
+    config.plugins.push(new webpack.EnvironmentPlugin({MapboxAccessToken: 'pk.eyJ1IjoiY2lwaGVyMDEiLCJhIjoiY2xnMzVvZXA1MGNmMDNzcHRjNmxnMHBqZyJ9.7_AMShwyXb_lsQI26LOUZw'}));
+    return config;
+  }
 }
 
 module.exports = nextConfig
--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <!-- Mapbox --> */}
+        <link href='https://api.tiles.mapbox.com/mapbox-gl-js/v1.5.0/mapbox-gl.css' rel='stylesheet' />
+
+        {/* <!-- Direction API --> */}
+        <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-directions/v4.1.0/mapbox-gl-directions.css"></link>
+      </Head>
       <body>
         <Main />
         <NextScript />
